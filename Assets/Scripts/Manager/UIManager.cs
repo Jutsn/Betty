ae7990938a1da160ab16e6436fedf2d1382c39e4
kikiyuuki <109820814+kikiyuuki@users.noreply.github.com>
2025-06-06 13:34:03 +0200

    battery PErcentage and color change

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -21,37 +21,64 @@ public class UIManager : MonoBehaviour
     private GameObject PressFPanel;
     [SerializeField]
     private GameObject PressQPanel;
+    public TMPro.TextMeshProUGUI batteryPercentageText;
+    public float batteryPercentage;
 
 
     private void Awake()
+  {
+    if (Instance == null)
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+      Instance = this;
+      DontDestroyOnLoad(gameObject);
     }
+    else
+    {
+      Destroy(gameObject);
+    }
+  }
 
     private void Start()
     {
         UpdateBatteryChargeUI();
     }
 
+  private void Update()
+  {
+    batteryPercentage = (batterySO.energy / batterySO.maxEnergy) * 100;
+      Debug.Log(batterySO.energy);
+    }
+
     // Update is called once per frame
-    public void UpdateBatteryChargeUI()
+  public void UpdateBatteryChargeUI()
+  {
+    batteryChargeSlider.maxValue = batterySO.maxEnergy;
+    batteryChargeSlider.value = batterySO.energy;
+    ChangeBatteryColor();
+    batteryPercentageText.text = $"{(batterySO.energy / batterySO.maxEnergy * 100).ToString("F0")}%";
+  }
+  
+    private void ChangeBatteryColor()
     {
-        batteryChargeSlider.maxValue = batterySO.maxEnergy;
-        batteryChargeSlider.value = batterySO.energy;
+      if(batteryPercentage > 50f)
+      {
+          batteryChargeSlider.fillRect.GetComponent<Image>().color = Color.green;
+      }
+      else if(batteryPercentage > 20f)
+      {
+          batteryChargeSlider.fillRect.GetComponent<Image>().color = Color.yellow;
+      }
+      if(batteryPercentage <= 20f)
+      {
+          batteryChargeSlider.fillRect.GetComponent<Image>().color = Color.red;
+      }
+
     }
 
     public void ShowChargeStationUI()
-    {
-        chargeStationPanel.SetActive(true);
-    }
+  {
+    chargeStationPanel.SetActive(true);
+  }
 
     public void HideChargeStationUI()
     {
