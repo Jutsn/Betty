bd6541ab8190898a56cac4fb8d66e24592ec5b2f
Jutsn <jusleyser@gmail.com>
2025-06-06 21:30:56 +0200

    .

--- Assets/Scripts/EndsceneCollider.cs
+++ Assets/Scripts/EndsceneCollider.cs
@@ -6,6 +6,6 @@ public class EndsceneCollider : MonoBehaviour
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if(collision.CompareTag("Player"))
-			SceneManager.LoadScene("Overworld",LoadSceneMode.Additive);
+			SceneManager.LoadScene("Overworld");
 	}
 }
